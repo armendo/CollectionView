0e5f47af711e60fca15689f8910a48de7222b277
armendo <Fer@Fers-MacBook-Pro.local>
2016-01-25 14:45:41 -0500

    Adding the Xcode project

---
+++ CollectionViewTutorial/ColorCell.swift
@@ -0,0 +1,14 @@
+//
+//  ColorCell.swift
+//  CollectionViewTutorial
+//
+//  Created by Fer on 1/25/16.
+//  Copyright © 2016 Fernando Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    @IBOutlet weak var colorLabel: UILabel!
+}
---
+++ CollectionViewTutorial/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  CollectionViewTutorial
+//
+//  Created by Fer on 1/25/16.
+//  Copyright © 2016 Fernando Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    @IBOutlet weak var collectionView: UICollectionView!
+    let totalColors: Int = 500
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource   =   self
+        collectionView.delegate     =   self
+        
+        
+        //Programatically Configurying the flow layout
+        flowLayout.scrollDirection = .Vertical
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 10)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Helper Methods
+    func colorForIndexPath(indexPath: NSIndexPath) -> UIColor {
+        if indexPath.row >= totalColors {
+            return UIColor.blackColor()	// return black if we get an unexpected row index
+        }
+        
+        var hueValue: CGFloat = CGFloat(indexPath.row) / CGFloat(totalColors)
+        return UIColor(hue: hueValue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+    }
+
+    //MARK: - UICollectionViewDataSource protocol methods
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return totalColors
+    }
+    
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    func collectionView(
+        collectionView: UICollectionView,
+        cellForItemAtIndexPath indexPath: NSIndexPath)
+        -> UICollectionViewCell{
+            
+        let cell    =   collectionView.dequeueReusableCellWithReuseIdentifier("colorCell", forIndexPath: indexPath) as! ColorCell
+        
+        let cellColor = colorForIndexPath(indexPath)
+        cell.backgroundColor = cellColor
+            
+        if CGColorGetNumberOfComponents(cellColor.CGColor) == 4 {
+            let redComponent = CGColorGetComponents(cellColor.CGColor)[0] * 255
+            let greenComponent = CGColorGetComponents(cellColor.CGColor)[1] * 255
+            let blueComponent = CGColorGetComponents(cellColor.CGColor)[2] * 255
+            cell.colorLabel.text = String(format: "%.0f, %.0f, %.0f", redComponent, greenComponent, blueComponent)
+        }
+        return cell
+    }
+
+
+}
+
